e818f6e76fc6c330c12a74116f994ac6bd6e2ee8
Felix A. Schultz <felix.schultz@engapho.com>
2022-11-08 22:29:34 +0100

    Installed Chart.js

---
+++ src/components/Charts/Pie/Pie.js
@@ -0,0 +1,29 @@
+/* import { Pie } from "react-chartjs-2"; */
+export default function PieChart(props) {
+    return (
+        <>
+            {/* <Pie
+                data={ props.data }
+                options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    legend: {
+                        position: 'top'
+                    },
+                    color: "white",
+                    title: {
+                        display: true,
+                        text: "Languages"
+                    },
+                    hoverOffset: 20,
+                    offset: 20,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        },
+                    }
+                }}
+            /> */}
+        </>
+    )
+}
