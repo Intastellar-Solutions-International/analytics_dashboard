49caecd9fab73f1f0ccd086b96ec517c899c23ed
Felix A. Schultz <felix.schultz@engapho.com>
2022-11-21 11:41:54 +0100

    Added Organisation view under users Name

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,29 @@
+const { useState, useEffect, useRef } = React;
 import "./header.css";
 import logo from "./logo.png";
+import Fetch from "../../functions/fetch";
+import API from "../../API/api";
+import Authentication from "../../Authentication/Auth";
 
 export default function Header() {
     const profileImage = JSON.parse(localStorage.getItem("globals"))?.profile?.image;
     const Name = JSON.parse(localStorage.getItem("globals"))?.profile?.name?.first_name + " " + JSON.parse(localStorage.getItem("globals"))?.profile?.name?.last_name;
     const email = JSON.parse(localStorage.getItem("globals"))?.profile?.email;
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        Fetch(API.settings.getOrganisation.url, API.settings.getOrganisation.method, API.settings.getOrganisation.headers, JSON.stringify({
+            organisationMember: Authentication.getUserId()
+        })).then((data) => {
+            if (data === "Err_Login_Expired") {
+                localStorage.removeItem("globals");
+                window.location.href = "/login";
+                return;
+            }
+            setData(data);
+        });
+    }, [])
 
     return (
         <>
@@ -13,7 +32,20 @@ export default function Header() {
                     <img className="dashboard-logo" src={ logo } alt="Intastellar Solutions Logo" />
                     <div className="flex">
                         <img src={profileImage} className="content-img"></img>
-                        <p className="dashboard-name">{Name}</p>
+                        <div>
+                            <p className="dashboard-name">{Name}</p>
+                            <select>
+                            {
+                                (!data) ? "" : data.map((d) => {
+                                    return (
+                                        <>
+                                            <option>{ d }</option>
+                                        </>
+                                    )
+                                })
+                            }
+                            </select>
+                        </div>
                     </div>
                 </div>
             </header>
