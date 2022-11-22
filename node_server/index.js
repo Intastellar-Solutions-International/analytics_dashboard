cb75af3ad8a03de6f569ff247fd63e9f61d4aff7
Felix A. Schultz <felix.schultz@engapho.com>
2022-11-22 16:22:52 +0100

    Setting up nodemon & node server

--- node_server/index.js
+++ node_server/index.js
@@ -4,9 +4,9 @@ const mysql = require("mysql");
 const app = express();
 
 app.get("/tr", (request, response) => {
-    response.send("Hi there")
+    response.send("Hi you are now on /tr")
 });
 
 app.listen(9000, () => {
-    console.log("Listen on port 9000")
+    console.log("Listen on port 9000");
 })
