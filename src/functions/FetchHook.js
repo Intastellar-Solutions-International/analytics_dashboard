d2b2aa920b6ca81a5883a96c3232e7aa7a83fe7d
Felix A. Schultz <felix.schultz@engapho.com>
2022-11-24 13:39:31 +0100

    Reduced fetch function on organisation page

--- src/functions/FetchHook.js
+++ src/functions/FetchHook.js
@@ -5,7 +5,8 @@ export default function useFetch(updateInterval, url, method, headers, body){
     const [error, setError] = useState();
     const [lastUpdated, setLastUpdated] = useState(Math.floor(Date.now() / 100));
     const [updated, setUpdated] = useState("");
-    
+    let id = undefined;
+
     useEffect(() => {
         const controller = new AbortController();
         setLoading(true);
@@ -18,28 +19,33 @@ export default function useFetch(updateInterval, url, method, headers, body){
                 setUpdated("Now");
                 setLastUpdated(Math.floor(Date.now() / 1000));
             });
+        
+        if(typeof(updateInterval) !=='undefined'){
+            const interval1 = setInterval(() => {
+                if ((Math.floor(Date.now() / 1000)) - lastUpdated >= 60) {
+                    setUpdated(Math.floor(((Math.floor(Date.now() / 1000)) - lastUpdated) / 60) + " minute ago");
+                }
+            }, 1000);
 
-        const interval1 = setInterval(() => {
-            if ((Math.floor(Date.now() / 1000)) - lastUpdated >= 60) {
-                setUpdated(Math.floor(((Math.floor(Date.now() / 1000)) - lastUpdated) / 60) + " minute ago");
-            }
-        }, 1000);
+            id = setInterval(() => {
+                fetch(url, { method: method, headers, body, signal: controller.signal } )
+                .then((res) => res.json())
+                .then(setData)
+                .catch(setError)
+                .finally(() => {
+                    setLoading(false);
+                    clearInterval(interval1);
+                    setUpdated("Now");
+                    setLastUpdated(Math.floor(Date.now() / 1000));
+                });
+            }, updateInterval * 60 * 1000)
+        }
 
-        const id = setInterval(() => {
-            fetch(url, { method: method, headers, body, signal: controller.signal } )
-            .then((res) => res.json())
-            .then(setData)
-            .catch(setError)
-            .finally(() => {
-                setLoading(false);
-                clearInterval(interval1);
-                setUpdated("Now");
-                setLastUpdated(Math.floor(Date.now() / 1000));
-            });
-        }, updateInterval * 60 * 1000)
         return () => {
             controller.abort();
-            clearInterval(id);
+            if(typeof(updateInterval) !=='undefined'){
+                clearInterval(id);
+            }
         }
     }, [url, lastUpdated]);
 
--- src/pages/Settings/ViewOrganisations/index.js
+++ src/pages/Settings/ViewOrganisations/index.js
@@ -1,4 +1,5 @@
 import Fetch from "../../../Functions/fetch";
+import useFetch from "../../../Functions/FetchHook";
 import API from "../../../API/api";
 import Authentication from "../../../Authentication/Auth";
 import Loading from "../../../Components/widget/Loading";
@@ -6,28 +7,18 @@ const { useState, useEffect, useRef } = React;
 const Link = window.ReactRouterDOM.Link;
 export default function ViewOrg() {
     document.title = "My Organisation | Intastellar Analytics";
-    const [data, setData] = useState(null);
 
-    useEffect(() => {
-        Fetch(API.settings.getOrganisation.url, API.settings.getOrganisation.method, API.settings.getOrganisation.headers, JSON.stringify({
-            organisationMember: Authentication.getUserId()
-        })).then((data) => {
-            if (data === "Err_Login_Expired") {
-                localStorage.removeItem("globals");
-                window.location.href = "/login";
-                return;
-            }
-            setData(data);
-        });
-    }, [])
-
+    const [loading, data, error, updated] = useFetch(1,API.settings.getOrganisation.url, API.settings.getOrganisation.method, API.settings.getOrganisation.headers, JSON.stringify({
+        organisationMember: Authentication.getUserId()
+    }))
+    
     return (
         <>
             <main className="dashboard-content">
                 <h1>My Organisation</h1>
                 <Link className="backLink" to="/settings">Back to settings</Link>
                 {
-                    (!data) ? <Loading /> : data.map((d, key) => {
+                    (loading) ? <Loading /> : data.map((d, key) => {
                         d = JSON.parse(d);
                         return (
                             <>
