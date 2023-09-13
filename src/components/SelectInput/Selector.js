7a099d889934de2e34c8a197bce6f36333a2d2b6
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-13 12:26:35 +0200

    Checking if the item is JSON or not

--- src/components/SelectInput/Selector.js
+++ src/components/SelectInput/Selector.js
@@ -2,31 +2,30 @@ import "./Style.css";
 const useParams = window.ReactRouterDOM.useParams;
 export default function Select(props){
     const { handle } = useParams();
-    
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
     return <>
         <div className="selector">
             {(props?.title) ? <label>{props.title}</label> : null}
             <select name={(handle) ? handle : props?.defaultValue} value={(handle) ? handle : props?.defaultValue} onChange={props.onChange}>
                 {
                     props?.items?.map((item, key) => {
-                        item = (typeof item === "string") ? JSON.parse(item) : item;
-                        if(typeof item === "string"){
+                        if(isJson(item)){
+                            item = JSON.parse(item);
+                            return <>
+                                <option key={item.id} value={JSON.stringify({ id: item.id, name: item.name })}>{item.name}</option>
+                            </>
+                            
+                        }else {
                             return <>
                                 <option key={item} value={item}>{item}</option>
-                            </>
-                        }else {
-                            if(item.name){
-                                return <>
-                                    <option key={item.id} value={JSON.stringify({ id: item.id, name: item.name })}>{item.name}</option>
-                                </>
-                            }else{
-                                let domain = (item[0] !== "") ? item[0] : null;
-                                return <>
-                                    
-                                    <option key={domain} value={domain}>{domain}</option>
-                                </>
-                            }
-                            
+                            </> 
                         }
                         
                     })
