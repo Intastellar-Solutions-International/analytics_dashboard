ab2f10fe6ea9e211e6dc9440dbc67120a4b9b4d0
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-18 17:07:37 +0200

    Added some info text about domain add.

--- src/components/AddDomain/AddDomain.js
+++ src/components/AddDomain/AddDomain.js
@@ -2,7 +2,9 @@ import Textfield from '../InputFields/textInput';
 export default function AddDomain(){
     return (
         <>
-            <h2>Add domain</h2> 
+            <h2>Add domain</h2>
+            <p>Here you can add a domain to your organisation.</p>
+            <p>After adding a domain you can implement the GDPR cookiebanner on your website.</p>
             <Textfield placeholder="Add domain" />
         </>
     )
