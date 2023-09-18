a3561642bae7be345669aa484f641299c7d805dc
Felix A. Schultz <felix.schultz@engapho.com>
2023-09-18 21:53:13 +0200

    Changed style of welcome banner

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -47,11 +47,9 @@ export default function Dashboard(props){
     return (
         <>
             <div className="dashboard-content">
-                <section style={{paddingTop: "20px"}}>
+                <section style={{padding: "40px", backgroundColor: "rgb(218, 218, 218)", color: "#626262"}}>
                     <h1>Welcome, {JSON.parse(localStorage.getItem("globals")).profile.name.first_name}</h1>
                     <p>Here you can see all the data regarding your GDPR cookiebanner implementation of your organisation</p>
-                </section>
-                <div style={{paddingTop: "40px"}}>
                     <h2 style={{display: "flex"}}>Organisation: {
                         <Select style={{marginLeft: "10px"}} defaultValue={organisation}
                         onChange={(e) => { 
@@ -59,7 +57,9 @@ export default function Dashboard(props){
                             localStorage.setItem("organisation", e);
                             window.location.reload();}}
                         items={organisations} title="Choose one of your domains"/>
-                        }</h2>
+                    }</h2>
+                </section>
+                <div>
                     {
                         (dashboardView === "GDPR Cookiebanner" && organisation != null &&  JSON.parse(organisation).id == 1) ? <TopWidgets dashboardView={dashboardView} API={{
                             url: API.gdpr.getTotalNumber.url,
