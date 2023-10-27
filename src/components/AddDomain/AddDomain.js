85b1bdd0a2f822ddb8a0f700ac24e020900dd376
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-27 10:01:35 +0200

    Created button component

--- src/components/AddDomain/AddDomain.js
+++ src/components/AddDomain/AddDomain.js
@@ -1,5 +1,8 @@
 import Textfield from '../InputFields/textInput';
+import Button from '../Button/Button';
+const { useState, useEffect, useRef, createContext } = React;
 export default function AddDomain(){
+    const [currentDomain, setCurrentDomain] = useState(null);
     return (
         <>
             <div className="dashboard-content">
@@ -7,6 +10,14 @@ export default function AddDomain(){
                 <p>Here you can add a domain to your organisation.</p>
                 <p>After adding a domain you can implement the GDPR cookiebanner on your website.</p>
                 <Textfield placeholder="Add domain" />
+                <Button onClick={(e) => {
+                    e.preventDefault();
+                    console.log("Add domain");
+                    setCurrentDomain(e.target.previousSibling.value);
+                }} text="Add domain" />
+                {
+                    (currentDomain == null) ? "" : <p>{currentDomain}</p>
+                }
             </div>
         </>
     )
