779e95530b0a324766594fbd46c0ba2f1c4a3fbb
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-28 14:27:04 +0200

    Feat: Added a form for the crawler input
    Added a new section on the page for the Cookie crawler to be placed

---
+++ src/Pages/Reports/SiteStatus/index.js
@@ -0,0 +1,17 @@
+                <div className="crawler-form">
+                    <TextInput placeholder="Enter Website" onChange={(e) => setWebsite(e.target.value)} />
+                    <Select defaultValue={defaultValue} key={""} items={domains?.map((d) => {
+                        return d.domain;
+                    })} onChange={
+                        (e) => {
+                            setWebsite(e);
+                            setDefaultValue(e);
+                        }
+                    } />
+                    <Button className="crawl-cta" onClick={crawlWebsite}>Crawl Website</Button>
+                </div>
+
+                {
+                    websiteStatus === "Crawled" && <p>Found {data?.length} cookies on your website.</p>
+                }
+
