6488fc8eedff4e880e7f2ec8b45976ee41967fdf
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-28 14:25:18 +0200

    Feat: Created Table Component

---
+++ src/components/Tabel/index.js
@@ -0,0 +1,33 @@
+import "./Style.css";
+export default function Table(props){
+    const data = props.data;
+    const headers = props.headers;
+    return <>
+        <article className="table">
+            <header className="table-header">
+                <div className="table-row">
+                    {
+                        headers.map((d, i) => {
+                            return <div className="table-cell" key={i}>{d}</div>
+                        })
+                    }
+                </div>
+            </header>
+            <section className="table-body">
+                {
+                    <div className="table-row">
+                        {
+                            data.map((d, i) => {
+                                return <>
+                                    <div className="table-cell" key={i}>{d.name}</div>
+                                    <div className="table-cell" key={i}>{d.value}</div>
+                                    <div className="table-cell" key={i}>{d.domain}</div>
+                                </>
+                            })
+                        }
+                    </div>
+                }
+            </section>
+        </article>
+    </>
+}
