cc96fcc3e9d8b3ef3f1987a5a02079bcee028720
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-28 14:39:40 +0200

    Some bugs

--- src/pages/Reports/SiteStatus/index.js
+++ src/pages/Reports/SiteStatus/index.js
@@ -1,3 +1,49 @@
+import SideNav from "../../../Components/Header/SideNav";
+import { reportsLinks } from "../Reports";
+const { useState, useEffect, useRef, createContext } = React;
+import Select from "../../../Components/SelectInput/Selector";
+import Button from "../../../Components/Button/Button";
+import Table from "../../../Components/Tabel";
+import TextInput from "../../../Components/InputFields/textInput";
+import "./Style.css";
+
+export default function SiteStatus({domains}) {
+    document.title = "Site Status | Intastellar Analytics";
+    const [websites, setWebsites] = useState([]);
+    const [website, setWebsite] = useState("");
+    const [websiteStatus, setWebsiteStatus] = useState("Not Crawled");
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+    const [defaultValue, setDefaultValue] = useState("or Select a Website");
+
+    function crawlWebsite(){
+        setWebsiteStatus("Crawling...");
+        setLoading(true);
+        fetch("https://apis.intastellarsolutions.com/crawl-website", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                url: "https://" + website
+            })
+        }).then(res => res.json()).then(res => {
+            if(res === "Err_Invalid_Website"){
+                setWebsiteStatus("Invalid Website");
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+            setWebsiteStatus("Crawled");
+            setData(res);
+        });
+    }
+
+    return <>
+        <SideNav links={reportsLinks} />
+        <div className="dashboard-content">
+            <h1>Site Status {websiteStatus}</h1>
+            <div className="form">
                 <div className="crawler-form">
                     <TextInput placeholder="Enter Website" onChange={(e) => setWebsite(e.target.value)} />
                     <Select defaultValue={defaultValue} key={""} items={domains?.map((d) => {
@@ -15,10 +61,28 @@
                     websiteStatus === "Crawled" && <p>Found {data?.length} cookies on your website.</p>
                 }
 
-                url: "https://" + website
-            if(res === "Err_Invalid_Website"){
-                setWebsiteStatus("Invalid Website");
-                setLoading(false);
-                return;
-            }
-            setWebsiteStatus("Crawled");
+                {loading && <p>Loading...</p>}
+                
+                {!loading && data?.length > 0 && <>
+                    <h3>Cookies found on your Website</h3>
+                    <Table headers={["Name", "Value", "Domain"]} data={data} />
+                    {/* <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Domain</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data?.map((d, i) => <tr key={i}>
+                                <td>{d.name}</td>
+                                <td>{d.domain}</td>
+                            </tr>)}
+                        </tbody>
+                    </table> */}
+                </>
+                }
+            </div>
+        </div>
+    </>;
+}
