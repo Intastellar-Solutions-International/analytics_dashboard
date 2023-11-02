030a89ffa5b701be378bcad691264f1c105710c0
Felix A. Schultz <felix.schultz@engapho.com>
2023-11-02 21:09:06 +0100

    Fix: Updated login screen a bit

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -18,13 +18,15 @@ export default function Login() {
             <div className="loginForm-container">
                 <form className="loginForm" onSubmit={(e) => { e.preventDefault(), Authentication.Login(API.Login.url, email, password, type, setErrorMessage, setLoading) }}>
                     <img className="loginForm-logo" src={logo} alt="Intastellar Solutions Logo" />
-                    <h1 className="loginForm-title">Signin to Intastellar Analytics</h1>
+                    <h1 className="loginForm-title">Sign in to Intastellar Analytics</h1>
                     <label>{(errorMessage != null) ? errorMessage : null }</label>
                     <label>Email:</label>
                     <input className="loginForm-inputField" type="email" placeholder="email" onChange={e => { setEmail(e.target.value); }} />
                     <label>Password:</label>
                     <input className="loginForm-inputField" type="password" placeholder="password" onChange={e => { setPassword(e.target.value); }} />
                     <button className="loginForm-inputField --btn" type="submit">{ (isLoading) ? "We are loggin you in..." : "SIGNIN" }</button>
+                    <a className="loginForm-inputField --link" href="/forgot-password">Forgot Password?</a>
+                    
                 </form>
             </div>       
         </>
