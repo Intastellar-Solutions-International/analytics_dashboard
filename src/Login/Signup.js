f96a4dd0f463daeb3d6847de51291b99aac4af97
Felix A. Schultz <felix.schultz@engapho.com>
2023-11-03 17:33:19 +0100

    Fix: Fixed signup form overflow on mobile
    Added overflow scroll to the signup page on mobile devices

--- src/Login/Signup.js
+++ src/Login/Signup.js
@@ -19,20 +19,21 @@ export default function Signup() {
 
     return <>
         <div className="loginForm-container">
-            <form className="loginForm" onSubmit={(e) => { e.preventDefault(), Authentication.SignUp(API.SignUp.url, email, password, firstname, lastname, type, companyName, setErrorMessage, setLoading) }}>
+            <form className="loginForm signup" onSubmit={(e) => { e.preventDefault(), Authentication.SignUp(API.SignUp.url, email, password, firstname, lastname, type, companyName, setErrorMessage, setLoading) }}>
                 <img className="loginForm-logo" src={logo} alt="Intastellar Solutions Logo" />
                 <h1 className="loginForm-title">Sign up to Intastellar Consents</h1>
                 <label>{(errorMessage != null) ? errorMessage : null }</label>
                 <label>Company Name:</label>
-                <input className="loginForm-inputField" type="text" placeholder="company name" onChange={e => { setCompanyName(e.target.value); }} />
+                <input className="loginForm-inputField" type="text" placeholder="Enter your company name here" onChange={e => { setCompanyName(e.target.value); }} />
                 <label>Firstname:</label>
-                <input className="loginForm-inputField" type="text" placeholder="firstname" onChange={e => { setFirstName(e.target.value); }} />
+                <input className="loginForm-inputField" type="text" placeholder="Enter your Firstname here" onChange={e => { setFirstName(e.target.value); }} />
                 <label>Lastname:</label>
-                <input className="loginForm-inputField" type="text" placeholder="firstname" onChange={e => { setLastName(e.target.value); }} />
+                <input className="loginForm-inputField" type="text" placeholder="Enter your Lastname here" onChange={e => { setLastName(e.target.value); }} />
                 <label>Email:</label>
-                <input className="loginForm-inputField" type="email" placeholder="email" onChange={e => { setEmail(e.target.value); }} />
+                <input className="loginForm-inputField" type="email" placeholder="Enter your Work email here" onChange={e => { setEmail(e.target.value); }} />
                 <label>Password:</label>
-                <input className="loginForm-inputField" type="password" placeholder="password" onChange={e => { setPassword(e.target.value); }} />
+                <input className="loginForm-inputField" type="password" placeholder="Create your password here" onChange={e => { setPassword(e.target.value); }} />
+                <input type="checkbox" id="tos"/><label htmlFor="tos">I accept <a href="https://www.intastellarsolutions.com/about/legal/terms" target="_blank">Terms and Conditions</a></label>
                 <button className="loginForm-inputField --btn" type="submit">{ (isLoading) ? "We are loggin you in..." : "SIGNUP" }</button>
                 <Link className="loginForm-inputField --link" to="/login">Have an account? Login</Link>
             </form>
