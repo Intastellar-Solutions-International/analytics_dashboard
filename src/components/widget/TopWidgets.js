94e5c5eeeec548682c87c92b76a74952a1a58c8e
Felix A. Schultz <felix.schultz@engapho.com>
2024-10-09 15:25:08 +0200

    refactor: Clean up TopWidgets component code and improve data handling

--- src/components/widget/TopWidgets.js
+++ src/components/widget/TopWidgets.js
@@ -1,7 +1,7 @@
 const { useState, useEffect, useRef, useContext } = React;
 import Widget from "./widget";
 import useFetch from "../../Functions/FetchHook";
-import {Loading} from "./Loading";
+import { Loading } from "./Loading";
 import ErrorBoundary from "../Error/ErrorBoundary";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -9,7 +9,7 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function TopWidgets(props) {
-    
+
     const APIUrl = props.API.url;
     const APIMethod = props.API.method;
     const APIHeader = props.API.header;
@@ -17,10 +17,10 @@ export default function TopWidgets(props) {
     const [loading, data, error, updated] = useFetch(30, APIUrl, APIMethod, APIHeader);
     if (data === "Err_Login_Expired") {
         localStorage.removeItem("globals");
-        window.location.href = "/#login";
+        window.location.href = "/login";
         return;
     }
-    
+
     const chartRef = useRef(ChartJS);
     const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
@@ -28,11 +28,11 @@ export default function TopWidgets(props) {
         <>
             <div className="grid-container grid-3">
                 {(loading) ? <Loading /> : <ErrorBoundary>
-                        <Widget overviewTotal={ true } totalNumber={ data?.Total.toLocaleString("de-DE") } type="Website" />
-                    </ErrorBoundary>
+                    <Widget overviewTotal={true} totalNumber={data?.Total?.toLocaleString("de-DE")} type="Website" />
+                </ErrorBoundary>
                 }
-                {(loading) ? <Loading /> : <ErrorBoundary><Widget totalNumber={ data?.JS.toLocaleString("de-DE") + "%" } type="JavaScript" /></ErrorBoundary> }
-                {(loading) ? <Loading /> : <ErrorBoundary><Widget totalNumber={ data?.WP.toLocaleString("de-DE") + "%" } type="WordPress" /></ErrorBoundary> }
+                {(loading) ? <Loading /> : <ErrorBoundary><Widget totalNumber={data?.JS?.toLocaleString("de-DE") + "%"} type="JavaScript" /></ErrorBoundary>}
+                {(loading) ? <Loading /> : <ErrorBoundary><Widget totalNumber={data?.WP?.toLocaleString("de-DE") + "%"} type="WordPress" /></ErrorBoundary>}
             </div>
         </>
     )
